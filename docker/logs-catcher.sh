82db50a9fc9838bfebe579a85e2c58b624e2bfe7
Frugan <dev@frugan.it>
2024-06-19 22:45:52 +0200

    build: attempt to use target_commitish w/ softprops/action-gh-release

--- docker/logs-catcher.sh
+++ docker/logs-catcher.sh
@@ -3,12 +3,20 @@
 CONTAINER_NAME=$1
 SEARCH_STRING=$2
 TIMEOUT=$3
+INTERVAL=5
 
-if timeout "$TIMEOUT" bash -c "
-    docker compose logs -f $CONTAINER_NAME | grep -m 1 '$SEARCH_STRING'
-"; then
-	echo "String '$SEARCH_STRING' found in '$CONTAINER_NAME' container logs."
-else
-	echo "Error: Timeout reached or string '$SEARCH_STRING' not found in '$CONTAINER_NAME' container logs."
-	exit 1
-fi
+elapsed_time=0
+
+while [ $elapsed_time -lt "$TIMEOUT" ]; do
+	if docker compose logs "$CONTAINER_NAME" | grep -m 1 "$SEARCH_STRING"; then
+		echo "String '$SEARCH_STRING' found in '$CONTAINER_NAME' container logs."
+		exit 0
+	fi
+
+	echo "Waiting for string '$SEARCH_STRING' in '$CONTAINER_NAME' container logs ($((TIMEOUT - elapsed_time)) seconds left to timeout)..."
+	sleep $INTERVAL
+	elapsed_time=$((elapsed_time + INTERVAL))
+done
+
+echo "Error: Timeout reached or string '$SEARCH_STRING' not found in '$CONTAINER_NAME' container logs."
+exit 1
